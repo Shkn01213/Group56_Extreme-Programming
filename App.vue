3d64eb3e8c2c3636666ec5d81fca5a7e40d6693c
S-jz7 <jiazbbb@gmail.com>
2024-11-23 13:00:48 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <ContactList />
+  </div>
+</template>
+
+<script>
+import ContactList from './components/ContactList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ContactList,
+  },
+};
+</script>
